34ee7a16edac4147a722637400ed86ab1db9cbe1
Lars Otterstad Veggeland <larsveggeland98@gmail.com>
2023-03-06 11:14:27 +0100

    Pipeline more or less done

---
+++ src/Pipeline.py
@@ -0,0 +1,252 @@
+#---------- Imported libraries ----------
+
+import pandas as pd
+import numpy as np
+from random import randrange, random, shuffle
+from copy import deepcopy
+from multiprocessing import Process, Queue
+from queue import Empty
+import json
+from DataPreProcessing import PreProcessor
+from Chunking import Chunking
+from Unmasking import Unmasking
+from Utils import get_time, print_progressbar
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+
+
+
+#---------- Pipeline ----------
+
+class Pipeline:
+     
+     def __init__(self, settings_file : str) -> None:
+        # Load settings from config file
+        with open(f"conf\\{settings_file}", "r") as file:
+            self.settings = json.load(file)
+        
+        if self.settings["build_author_curves"] == True:
+            # Load dataset from disk
+            print(f"{get_time()} - Getting data from disk...")
+            dataset = self.get_data(self.settings["dataset"])
+            authors = dataset["authors"].to_list()
+            files = dataset["articles"].to_list()
+
+            # Preprocess the loaded data
+            print(f"{get_time()} - Pre-processing files...")
+            proc = PreProcessor(jobs=self.settings["preprocessing_jobs"])
+            files = self.pre_process_data(data=files, proc=proc)
+
+            # Get X and A pairs
+            grouped_by_author = self.group_files_and_authors(authors, files)
+            self.chunker = Chunking(size=self.settings["chunk_size"], type=self.settings["chunk_type"])
+            if self.settings["balanced_classes"] == True:
+                X_A = self.create_X_A_pairs_balanced(grouped_by_author)
+            else:
+                X_A = self.create_X_A_pairs(grouped_by_author)
+
+            # Specify the Unmasking model
+            clf = Unmasking(
+                        chunker=self.chunker,
+                        normalized_features=self.settings["normalized_features"],
+                        number_of_features=self.settings["number_of_features"],
+                        C_parameter_curve_construction=self.settings["C_parameter_curve_construction"],
+                        C_parameter_curve_classification=self.settings["C_parameter_curve_classification"],
+                        kernel_type_curve_classification=self.settings["kernel_type_curve_classification"],
+                        build_author_curves=self.settings["build_author_curves"],
+                        save_author_curves=self.settings["save_author_curves"],
+                        load_author_curves=self.settings["load_author_curves"]
+                    )
+            
+            self.author_curves = [None]*len(X_A)
+
+            # Partition the X_A pairs into 10 blocks of equal length
+            blocks = [X_A[i::10] for i in range(10)]
+
+            # Use the blocks to run the author curve generation in parallell
+            print(f"{get_time()} - Building author curves...")
+            print_progressbar(current_position=-1, length=len(X_A))
+            processes = []
+            queue = Queue()
+            for i in range(10):
+                proc = Process(target=self.handle_block, args=(queue, blocks[i], clf))
+                processes.append(proc)
+            
+            for proc in processes:
+                proc.start()
+            
+            counter = 0
+            self.author_curves = [None]*len(X_A)
+            self.labels = np.zeros(len(X_A))
+        
+            while counter < len(X_A):
+                try:
+                    label_and_curve = queue.get(timeout=3)
+                    if label_and_curve is not None:
+                        self.labels[counter] = int(label_and_curve[0])
+                        self.author_curves[counter] = label_and_curve[1]
+                        print_progressbar(current_position=counter, length=len(X_A))
+                        counter += 1
+                except Empty:
+                    # The queue is empty and has not been provided with new results
+                    # This is ignored
+                    pass
+            
+            print(f"{get_time()} - Author curves have been sucesfully constructed")
+
+        if self.settings["save_author_curves"] and self.settings['build_author_curves']:
+            print(f"{get_time()} -Saved the created author curves to {self.settings['save_author_curves']}")
+            self.save_author_curves(self.settings["save_author_curves"], labels, np.array(author_curves))
+        
+        if self.settings["load_author_curves"] is not None:
+            if self.settings["load_author_curves"] != self.settings['save_author_curves'] and self.settings['build_author_curves']:
+                print("WARNING!\nYou are currently not verifying the performance of the author curves just constructed.")
+                print(f"You are saving the curves to {self.settings['save_author_curves']} but loading curves from another file {self.settings['load_author_curves']} for curve classification")
+                exit(1)
+            
+            print(f"{get_time()} - Author curves loaded from {self.settings['load_author_curves']}")
+            self.labels, self.author_curves = self.load_author_curves(self.settings["load_author_curves"])
+
+
+            # Train the author curve verification model
+            accuracy = self.classify_curves(
+                    kernel=self.settings["kernel_type_curve_classification"],
+                    C=self.settings["C_parameter_curve_classification"]
+                )
+            print(f"Accuracy for the author curve classifier: {round(accuracy*100, 2)} %")
+        
+
+
+
+
+     def get_data(self, filename : str) -> pd.DataFrame:
+        try:
+            return pd.read_csv(filename, encoding="utf-8", encoding_errors="ignore")
+        except FileNotFoundError:
+            print(f"The file {filename} could not be found")
+            exit(1)
+
+
+     def pre_process_data(self, data : list, proc : PreProcessor) -> list:
+         """
+         Perform the specified preprocessing on all files
+         """
+         processed_files = [proc.process_object(file) for file in data] 
+         return processed_files
+     
+
+     def group_files_and_authors(self, authors : list, files : list) -> dict:
+        """
+        Returns a dictionary where each author is mapped to all files (s)he has written
+        """
+        grouped_by_author = {author : [] for author in authors}
+        for i, author in enumerate(authors): grouped_by_author[author].append(files[i])
+        return grouped_by_author
+     
+
+     def create_X_A_pairs(self, grouped_by_author : dict) -> list:
+        """
+        Creates all possible book author pairs from the corpus
+        """
+        X_A = []
+        for author in grouped_by_author.keys():
+            files = grouped_by_author[author]
+            for i, file in enumerate(files):
+                # Chunk the file (X) here to avoid duplicate work later
+                chunked_file = self.chunker.chunk_file(file)
+                for a in grouped_by_author.keys():
+                    if a == author:
+                        if len(files) > 1:
+                            copied_files = deepcopy(files)
+                            copied_files.pop(i)
+                            X_A.append([True, file, chunked_file, copied_files])
+                    else:
+                        X_A.append([False, file, chunked_file, grouped_by_author[a]])
+        
+        return X_A
+
+
+     def create_X_A_pairs_balanced(self, grouped_by_author : dict) -> list:
+        same = []
+        different = []
+        for author in grouped_by_author.keys():
+            files = grouped_by_author[author]
+            for i, file in enumerate(files):
+                # Chunk the file (X) here to avoid duplicate work later
+                chunked_file = self.chunker.chunk_file(file)
+                for a in grouped_by_author.keys():
+                    if a == author:
+                        if len(files) > 1:
+                            copied_files = deepcopy(files)
+                            copied_files.pop(i)
+                            same.append([True, file, chunked_file, copied_files])
+                    else:
+                        different.append([False, file, chunked_file, grouped_by_author[a]])
+
+        # Ensure that there are equally many samples of each class
+        if len(different) > len(same):
+            shuffle(different)
+            different = different[:len(same)]
+        else:
+            shuffle(same)
+            same = same[:len(different)]
+
+        X_A = same + different
+        shuffle(X_A)
+        return X_A
+        
+
+     def handle_block(self, queue : Queue, block : list, clf : Unmasking) -> None:
+        """
+        Constructs the author curves from the provided block of X A pairs 
+        """
+        for X_A_pair in block:
+            # Get the different elements in the X_A pair
+            label = X_A_pair[0]
+            X = X_A_pair[1]
+            chunks_X = X_A_pair[2]
+            A = X_A_pair[3]
+
+            # Get the author curve
+            author_curve = clf.handle_X_A_pair(X=X, A=A, chunks_X=chunks_X)
+
+            # Now push the label and finished author cure to the que
+            queue.put([label, author_curve])
+
+    
+     def save_author_curves(self, filename : str, labels : np.ndarray, author_curves : np.ndarray) -> None:
+        """
+        Saves the provided author curves to a .csv file
+        """
+        # Combine the labels and author curves into one array and save it to a .csv file
+        c = np.concatenate((labels.reshape(-1, 1), author_curves), axis=1)
+        columns = ["label"] + [f"feature{i+1}" for i in range(author_curves.shape[1])]
+        df = pd.DataFrame({columns[i] : c[:,i] for i in range(len(columns))})
+        df.to_csv(filename, index=False)
+
+
+     def load_author_curves(self, filename : str) -> tuple:
+         """
+         Loads the labels and author curves from the specified .csv file
+         """
+         df = pd.read_csv(filename)
+         labels = df['label'].to_numpy()
+         author_curves = df.iloc[:,1:].to_numpy()
+         
+         return (labels, author_curves)
+
+    
+     def classify_curves(self, kernel : str, C : float) -> None:
+        """
+        Classifies the author curves and returns the average performance
+        from 5-fold cross-validation
+        """
+        # Specify, train, and test the model
+        clf = SVC(kernel=kernel, C=C)
+        scores = cross_val_score(clf, self.author_curves, self.labels, cv=5)
+        return scores.mean()
+
+
+
+if __name__ == "__main__":      
+    p = Pipeline("default.json")
