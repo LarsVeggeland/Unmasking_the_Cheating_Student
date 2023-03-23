d271e22de8cad0651fd1d25db1a022688d56c5d5
Lars Otterstad Veggeland <larsveggeland98@gmail.com>
2023-03-23 18:33:57 +0100

    Created skeleton for config validation

---
+++ src/ConfigValidator.py
@@ -0,0 +1,31 @@
+# ---------- Imported libraries --------
+
+
+
+# ---------- Util functions ----------
+
+def vaildate_dataset_fields(data : list) -> list:
+    # TODO
+    pass
+
+
+def validate_chunk_fields(conf : dict) -> list:
+    # TODO
+    pass
+
+
+def validate_feature_extractor_fields(conf : dict) -> list:
+    # TODO
+    pass
+
+
+def validate_curve_related_fields(conf : dict) -> list:
+    # TODO
+    pass
+
+
+# ---------- Configuration file validation ---------
+
+def validate_config_file(filename : str) -> None:
+    # TODO
+    pass
