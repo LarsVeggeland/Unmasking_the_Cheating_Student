b7decfa9dd4fc7f752f34e8a8d6f5edcdfdd19c9
Lars Otterstad Veggeland <larsveggeland98@gmail.com>
2023-03-23 18:47:05 +0100

    Created main file for running config files

---
+++ src/Main.py
@@ -0,0 +1,48 @@
+# ---------- Imported libraries ----------
+
+from os import listdir, path
+from Pipeline import Pipeline
+
+
+
+# ---------- Util functions ----------
+
+def run_config(filename : str) -> None:
+    """
+    Configures and runs an instance of the Pipeline
+    based on the provided config file
+    """
+    Pipeline(settings_file=filename)
+
+
+def run_all_config_files_in_dir(dirname : str) -> None:
+    """
+    Configures and runs a Pipeline instance
+    for each of the config files in the 
+    provided directory
+    """
+    config_files = listdir(dirname)
+    for file in config_files:
+        if file[-6:] != ".json":
+            pass
+        run_config(dirname + "/" + file)
+
+    
+
+# ---------- Main ----------
+
+def __main__(source : str) -> None:
+    """
+    Main function
+    """
+    if path.isdir(source):
+        run_all_config_files_in_dir(source)
+    elif path.isfile(source):
+        run_config(source)
+    else:
+        print(f"ERROR: {source} is neither a file nor directory")
+        exit(1)
+    
+
+if __name__ == "__main__":
+    __main__("conf\PreliminaryTesting")
