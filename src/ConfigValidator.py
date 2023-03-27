b2dc2f8d04fb3fc0f6f308ae98204306edbf8c45
larsveggeland <larsveggeland@gmail.com>
2023-03-27 09:38:42 +0200

    Fixed issue with assertion structures

--- src/ConfigValidator.py
+++ src/ConfigValidator.py
@@ -11,46 +11,44 @@ def vaildate_dataset_fields(dataset, word_cap, file_partitions, balanced_classes
     """
     errors = []
     try:
-        assert(isinstance(dataset, str))
-        assert(len(dataset) > 0)
-    except AssertionError:
-        errors.append(f"No datatset specified")
-    
-    try:
+        assert isinstance(dataset, str), f"No datatset specified"
+        assert len(dataset) > 0, f"No datatset specified"
         with open(dataset, "r") as file:
             pass
+    except AssertionError:
+        errors.append(f"No datatset specified")
     except FileNotFoundError:
         errors.append(f"The provided dataset {dataset} could not be found in the filesystem")
 
     try:
-        assert(dataset[-4:] == ".csv")
+        assert dataset[-4:] == ".csv"
     except AssertionError:
         errors.append(f"The provided dataset {dataset} is not a csv file")
 
     try:
-        assert(word_cap is None or isinstance(word_cap, int))
+        assert word_cap is None or isinstance(word_cap, int)
     except AssertionError:
         errors.append(f"The word cap must be null or an integer not {type(word_cap)}")
 
     try:
         if isinstance(word_cap, int):
-            assert(word_cap > 0)
+            assert word_cap > 0
     except AssertionError:
         errors.append(f"If not null the word cap must be a positive integer not {word_cap}")
 
     try:
-        assert(isinstance(file_partitions, bool))
+        assert isinstance(file_partitions, bool)
     except AssertionError:
         errors.append(f"The field 'file_partitions' must be defined as either true or false not {file_partitions} of type {type(file_partitions)}")
     
     try:
         if not isinstance(word_cap, int):
-            assert(file_partitions == False)
+            assert file_partitions == False
     except AssertionError:
         errors.append(f"Cannot set 'file_partitions' to true when when no word cap is enforced")
     
     try:
-        assert(isinstance(balanced_classes, bool))
+        assert isinstance(balanced_classes, bool)
     except AssertionError:
         errors.append(f"The field 'balanced_classes' must be defined as either true or false not {balanced_classes} of type {type(balanced_classes)}")
 
@@ -64,18 +62,18 @@ def validate_chunk_fields(conf : dict) -> list:
     errors = []
     try:
         size = conf["size"]
-        assert(size > 0)
+        assert size > 0
     except AssertionError:
         errors.append(f"Chunk size must be a positive integer not {size}")
 
     try:
         type = conf['type']
-        assert(type in ["words", "ngrams", "pos_tags", "lex_pos"])
+        assert type in ["words", "ngrams", "pos_tags", "lex_pos"] 
 
         if type == "ngrams":
             try:
                 ngram_size = conf["ngram_size"]
-                assert(ngram_size > 0 and isinstance(ngram_size, int))
+                assert ngram_size > 0 and isinstance(ngram_size, int)
             except AssertionError:
                 errors.append(f"ngram size must be a positive integer not {ngram_size}")
             except KeyError:
@@ -85,7 +83,7 @@ def validate_chunk_fields(conf : dict) -> list:
 
     try:
         method = conf["method"]
-        assert(method in ["original", "bootstrap", "sliding_window"])
+        assert method in ["original", "bootstrap", "sliding_window"]
     except AssertionError:
         errors.append(f"The provided chunk method {method} does not exits")
     
@@ -99,7 +97,7 @@ def validate_feature_extractor_fields(conf : dict) -> list:
     errors = []
     try:
         type = conf["type"]
-        assert(type in ["words", "ngrams", "pos_tags", "lex_pos"])
+        assert type in ["words", "ngrams", "pos_tags", "lex_pos"]
     except AssertionError:
         errors.append(f"The provided feature type {type} does not exist")
     except KeyError:
@@ -107,7 +105,7 @@ def validate_feature_extractor_fields(conf : dict) -> list:
 
     try:
         normalized = conf["normalized"]
-        assert(isinstance(normalized, bool))
+        assert isinstance(normalized, bool)
     except AssertionError:
         errors.append(f"The field normalized must be a boolean not {type(normalized)}")
     except KeyError:
@@ -115,7 +113,7 @@ def validate_feature_extractor_fields(conf : dict) -> list:
             
     try:
         number = conf["number"]
-        assert(isinstance(number, int) and number > 0)
+        assert isinstance(number, int) and number > 0 
     except AssertionError:
         errors.append(f"The field number must be a positive integer not {number}")
     except KeyError:
@@ -126,30 +124,30 @@ def validate_curve_related_fields(build, eliminate, save, load, C_cons, C_class,
     errors = []
 
     try:
-        assert(isinstance(build, bool))
+        assert isinstance(build, bool) 
     except AssertionError:
         errors.append(f"The field 'file_partitions' must be defined as either true or false not {build} of type {type(build)}")
 
     try:
-        assert(isinstance(eliminate, int))
+        assert isinstance(eliminate, int) 
     except AssertionError:
         errors.append(f"The field 'elimnate' must be an integer, not {type(eliminate)}")
 
     try:
         if isinstance(eliminate, int):
-            assert(eliminate > 0)
+            assert eliminate > 0 
     except AssertionError:
         errors.append(f"The field 'eliminate_features' must be a positive integer not {eliminate}. Cannot remove a negative number of features...")
     
     try:
-        assert(isinstance(build, bool))
+        assert isinstance(build, bool)
     except AssertionError:
         errors.append(f"The field 'build_author_curves' must be a boolean, not {type(build)}")
 
     try:
-        assert(isinstance(load, str), f"The field specifying the source file for loading the author curves must be a string not {type(save)}")
-        assert(len(load) > 4, "The source file for loading the author curves is not defined")
-        assert(load[-4:] == ".csv", f"The provided source file for the loaded author curves {load} is not a csv file")
+        assert isinstance(load, str), f"The field specifying the source file for loading the author curves must be a string not {type(save)}"
+        assert len(load) > 4, "The source file for loading the author curves is not defined"
+        assert load[-4:] == ".csv", f"The provided source file for the loaded author curves {load} is not a csv file" 
         if not build:
             with open(load, "r") as file:
                 pass
@@ -160,31 +158,31 @@ def validate_curve_related_fields(build, eliminate, save, load, C_cons, C_class,
 
     try:
         if build:
-            assert(isinstance(save, str), f"The field specifying the target file for saving the created author curves must be a string not {type(save)}")
-            assert(len(save) > 4, "The target file for saving the created author curves is not defined")
-            assert(save[-4:] == ".csv", f"The provided target file for saving the author curves {save} is not a csv file")
-            assert(save == load, f"Inconsistency between the author curves created and the ones used during testing. Not loading the created author curves {save} =/= {load}")
+            assert isinstance(save, str), f"The field specifying the target file for saving the created author curves must be a string not {type(save)}"
+            assert len(save) > 4, "The target file for saving the created author curves is not defined"
+            assert save[-4:] == ".csv", f"The provided target file for saving the author curves {save} is not a csv file"
+            assert save == load, f"Inconsistency between the author curves created and the ones used during testing. Not loading the created author curves {save} =/= {load}"
     except AssertionError as e:
         errors.append(e)
 
     try:
-        assert(isinstance(C_cons, float), f"The field 'C_parameter_curve_construction' must be a float not {type(C_cons)}")
+        assert isinstance(C_cons, float), f"The field 'C_parameter_curve_construction' must be a float not {type(C_cons)}"
         # C can be larger than 1, but that would be weird...
-        assert(C_cons <= 1 and C_cons >= 0, f"The field 'C_parameter_curve_construction' must be in the range [0, 1] not {C_cons}")    
+        assert C_cons <= 1 and C_cons >= 0, f"The field 'C_parameter_curve_construction' must be in the range [0, 1] not {C_cons}"   
     except AssertionError as e:
         errors.append(e)
 
     try:
-        assert(isinstance(C_class, float), f"The field 'C_parameter_curve_classification' must be a float not {type(C_cons)}")
+        assert isinstance(C_class, float), f"The field 'C_parameter_curve_classification' must be a float not {type(C_cons)}"
         # C can be larger than 1, but that would be weird...
-        assert(C_class <= 1 and C_class >= 0, f"The field 'C_parameter_curve_classification' must be in the range [0, 1] not {C_class}")    
+        assert C_class <= 1 and C_class >= 0, f"The field 'C_parameter_curve_classification' must be in the range [0, 1] not {C_class}"    
     except AssertionError as e:
         errors.append(e)
 
     try:
-        assert(isinstance(kernel, str, f"The field 'kernel_type_curve_classification' must be a string, not {type(kernel)}"))
+        assert isinstance(kernel, str, f"The field 'kernel_type_curve_classification' must be a string, not {type(kernel)}")
         allowed =  ['linear', "rbf", "poly", "sigmoid", "precomputed"]
-        assert(kernel in allowed, f"The field 'kernel_type_curve_classification' must be among {' '.join(allowed)}, not {kernel}")
+        assert kernel in allowed, f"The field 'kernel_type_curve_classification' must be among {' '.join(allowed)}, not {kernel}"
     except AssertionError as e:
         errors.append(e)
     
